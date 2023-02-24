c1426a4c61be818d3429ff63a6fae3c5129ed560
Knowswhere <126259288+Knowswhere@users.noreply.github.com>
2023-02-24 10:02:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -11,28 +11,49 @@ const cover = document.getElementById('cover');
 const currTime = document.querySelector('#currTime');
 const durTime = document.querySelector('#durTime');
 
+console.log("Happy New Year from the Software Team!\n");
+
 const songs = [
-  'Doja_Cat_-_Kiss_Me_More',
-  'Drivers_License_-_Olivia_Rodrigo',
-  'Lil_Nas_X_-_Montero',
-  'Glass_Animals_-_Heat_Waves',
-  'Good_4_U',
-  'Justin_Bieber_-_Peaches',
-  'Lil_Nas_X_-_Industry_Baby',
-  'Masked_Wolf_-_Astronaut_in_the_Ocean',
-  'The_Kid_Laroi_and_Justin_Bieber_-_Stay (1)',
-  'The_Weeknd_-_Blinding_Lights'
+  'BTS - Yet to come',
+  'Taeyeon – INVU',
+  'PSY - That That',
+  'IVE - Love Dive',
+  'NewJeans - Hype Boy',
+  'Blackpink Pink Venom',
+  'Le Sserafim - Antifragile',
+  'NewJeans - Attention',
+  'G-IDLE - Tomboy',
+  'IVE - After Like',
+  'Newjeans - ditto',
+  'TWICE - talk that talk',
+  'Taeyang - Vibe',
+  'Blackpink - Shut Down',
+  'Tomorrow x Together - Sugar Rush Ride',
+  'Newjeans - OMG',
+  'Kara -  When I Move',
+  'Ghost Rule - DECO*27',
+  'Haan Main Galat',
+  'Roman s Revenge',
+  'I cant handle change',
+  'Yung Gravy_Dillon Francis - Hot Tub (ft. T-Pain) ',
+  "Exo - Growl",
+  "Le Sserafim - Fearless",
+  "Stray Kids - MANIAC",
+  "Six Lounge - kitakaze"
 ];
 
-let songIndex = 2;
+//Index of the song it begins with (Blinding Lights)
+let songIndex = 10;
 
 loadSong(songs[songIndex]);
+
 
 function loadSong(song) {
   title.innerText = song;
   audio.src = `music/${song}.mp3`;
-  // audio.src = `music/${song}.mp3`;
   cover.src = `images/${song}.jpg`;
+  pauseSong();
+  playSong();
 
   console.log(song);
 }
@@ -108,5 +129,22 @@ function setProgress(e) {
 
   audio.currentTime = (clickX / width) * duration;
 }
+
+//Here for shuffle //Change the shuffle to have a button display somewhere on screen
+
+// const shuffleSongs = songs => {
+  // for (let i = songs.length - 1; i > 0; i--) {
+    // const j = Math.floor(Math.random()) * (i +1);
+    // const temp = songs[i];
+    // songs[i] = songs [i];
+  // songs[j] = temp;
+  
+  // }
+
+  // return songs;
+  
+ // };
+
+//Above is the code in comment for testing (if its wrong just delete it)
 
 audio.addEventListener('ended', nextSong);
